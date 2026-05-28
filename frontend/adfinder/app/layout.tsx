c481080be2304c0f2eb81f2ecd7354ed42921dce
h-aliyari <haniyealiyari1381@gmail.com>
2026-05-28 09:45:55 +0330

    fix ad import

--- frontend/adfinder/app/layout.tsx
+++ frontend/adfinder/app/layout.tsx
@@ -4,9 +4,6 @@ import "./globals.css";
 import Header from '../components/header/Header';
 import Footer from '../components/Footer';
 
-import PopupAd from "./ads/PopupAd";
-import BottomAd from "./ads/BottomAd";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fa">
